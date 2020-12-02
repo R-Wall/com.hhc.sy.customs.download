7eca29f4d72321e34e35a24c35f12498b9d58f85
Administrator <Administrator@PLMSERVERcopy>
2020-12-02 21:05:23 +0800

    添加排序功能

---
+++ src/com/hhc/sy/custonms/util/SortTask.java
@@ -0,0 +1,39 @@
+package com.hhc.sy.custonms.util;
+
+import java.util.Comparator;
+import java.util.Date;
+
+import com.teamcenter.rac.kernel.TCComponent;
+import com.teamcenter.rac.kernel.TCException;
+
+/**
+ * 升序排序
+ * @author Administrator
+ *
+ */
+public class SortTask implements Comparator<Object> {
+
+	public int compare(Object o1, Object o2) {
+		TCComponent t1 = (TCComponent) o1;
+		TCComponent t2 = (TCComponent) o2;
+		Date d1 = null;
+		Date d2 = null;
+		try {
+			d1 = t1.getDateProperty("fnd0EndDate");
+			d2 = t2.getDateProperty("fnd0EndDate");
+		} catch (TCException e) {
+			e.printStackTrace();
+		}
+		if (d1 == null || d2 == null) {
+			return 0;
+		}	
+		if (d2.after(d1)){
+			return 1;
+		} else if (d2.before(d1)){
+			return -1;
+		} else {
+			return 0;
+		}
+		
+	}
+}
